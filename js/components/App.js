c39281808dbd1160f4e00c426521276fe6cdc7ca
khoiruuuls <2010631170092@student.unsika.ac.id>
2024-09-03 13:50:36 +0700

    Episode 6 - Component Props

---
+++ js/components/App.js
@@ -0,0 +1,9 @@
+// Mengimpor komponen AppButton dari file eksternal
+import AppButton from "./AppButton.js";
+export default {
+  // Mendefinisikan aplikasi Vue dengan komponen yang terdaftar
+  components: {
+    // Mendaftarkan komponen 'app-button' yang diimpor
+    "app-button": AppButton,
+  },
+};
--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,21 +1,31 @@
 export default {
-    // Mendefinisikan template HTML untuk komponen 'app-button'
-    template: `
+  // Mendefinisikan template HTML untuk komponen 'app-button'
+  template: `
       <!-- Template untuk komponen 'app-button' -->
-      <!-- Tombol dengan styling menggunakan Tailwind CSS -->
-      <button class="bg-blue-400 hover:bg-blue-600 text-white px-5 py-2 rounded-full font-bold disabled:cursor-not-allowed"
+      <button :class="{
+      'px-5 py-2 rounded-full font-bold disabled:cursor-not-allowed': true,
+      'bg-blue-400 hover:bg-blue-600 text-white': type === 'primary',
+      'bg-green-400 hover:bg-green-600 text-white': type === 'secondary',
+      'bg-gray-400 hover:bg-gray-600 text-white': type === 'muted',
+      'is-loading': processing,
+         
+      }"
       :disabled="processing">
         <!-- Konten tombol diambil dari slot -->
         <slot/>
       </button>
     `,
-    
-    // Mendefinisikan properti data reaktif untuk komponen ini
-    data() {
-      return {
-        // Status 'processing' yang mengatur apakah tombol dalam keadaan dinonaktifkan
-        processing: true,
-      };
-    }
-  }
-  
+
+  props: {
+    // Mendefinisikan properti 'type' yang mengatur tipe tombol
+    type: {
+      type: String,
+      default: "primary",
+    },
+    // Mendefinisikan properti 'processing' yang mengatur status tombol
+    processing: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
